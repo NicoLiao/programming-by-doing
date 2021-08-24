864c900aa7b5b82c20523045d584429e0856789d
NicoLiao <89239543+NicoLiao@users.noreply.github.com>
2021-08-24 14:09:50 +0800

    add file

---
+++ BasicAndPrinting/ALetterToYourself/ALetterToYourself.java
@@ -0,0 +1,20 @@
+//BasicAndPrinting ALetterToYourself 7pts Nico Liao
+public class ALetterToYourself {
+
+    public static void main (String[] args)
+    {
+    System.out.println("+------------------------------+");
+    System.out.println("|                          ####|");
+    System.out.println("|                          ####|");
+    System.out.println("|                          ####|");
+    System.out.println("|                              |");
+    System.out.println("|                              |");
+    System.out.println("|                     Nico Liao|");
+    System.out.println("|               1 Microsoft Way|");
+    System.out.println("|                       Hsinchu|");
+    System.out.println("|------------------------------|");
+
+
+
+    }
+}
---
+++ BasicAndPrinting/YourInitials/YourInitials.java
@@ -0,0 +1,11 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println("N   N L");
+        System.out.println("NN  N L");
+        System.out.println("N N N L");
+        System.out.println("N N N L");
+        System.out.println("N  NN L");
+        System.out.println("N   N L");
+        System.out.println("N   N LLLLLL");
+    }
+}
