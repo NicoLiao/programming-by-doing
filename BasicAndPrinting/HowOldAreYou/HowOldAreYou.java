8d62465f51b8d060dd632e38398c18498204ca9c
NicoLiao <89239543+NicoLiao@users.noreply.github.com>
2021-09-01 21:50:06 +0800

    add file

---
+++ BasicAndPrinting/HowOldAreYou/HowOldAreYou.java
@@ -0,0 +1,22 @@
+//BasicAndPrinting HowOldAreYou 25pts Nico Liao
+import java.util.Scanner;
+public class HowOldAreYou {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        int age;
+        System.out.print( "How old are you? " );
+		age = keyboard.nextInt();
+        if (age < 16) {
+            System.out.println("You can't drive");
+        }
+        if (age < 18) {
+            System.out.println("You can't vote");
+        }
+        if (age < 25) {
+            System.out.println("You can't rent a car");
+        }
+        if (age >= 25) {
+            System.out.println("You can do anything that's legal");
+        }
+    }
+}
