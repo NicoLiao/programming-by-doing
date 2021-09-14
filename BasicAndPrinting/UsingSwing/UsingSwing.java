ae983739afc212d70077d46f70acabf3d502f19b
NicoLiao <89239543+NicoLiao@users.noreply.github.com>
2021-09-14 13:39:42 +0800

    add file

---
+++ BasicAndPrinting/UsingSwing/UsingSwing.java
@@ -0,0 +1,12 @@
+//BasicAndPrinting UsingSwing 25pts Nico Liao
+import javax.swing.*;
+public class UsingSwing {
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog("What is your name?");
+        String input = JOptionPane.showInputDialog("How old are you");
+        int age = Integer.parseInt(input);
+        System.out.print("Hello " + name + ".");
+        System.out.println(" Next year, you'll be " + (age+1));
+        System.exit(0);
+    }
+}
