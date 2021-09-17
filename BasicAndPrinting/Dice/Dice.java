87238d81fa6c5525ead3def1652122b286fa1e29
NicoLiao <89239543+NicoLiao@users.noreply.github.com>
2021-09-17 13:38:52 +0800

    add file

---
+++ BasicAndPrinting/Dice/Dice.java
@@ -0,0 +1,15 @@
+//BasicAndPrinting Dice 25pts Nico Liao
+import java.util.Random;
+public class Dice {
+    public static void main(String[] args) {
+        Random r = new Random();
+        int x = 1 + r.nextInt(6);
+        int y = 1 + r.nextInt(6);
+        int z = x + y;
+        System.out.println("First roll " + x);
+        System.out.println("Second roll " + y);
+        System.out.println("Total " + z);
+
+
+    }
+}
