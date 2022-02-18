6d4d56275c2c74e50f0d9c4852b683855a8be5a5
NicoLiao <89239543+NicoLiao@users.noreply.github.com>
2022-02-18 13:44:33 +0800

    add file

---
+++ C++/Conversion3/BasicNestedLoop/BasicNestedLoop.cpp
@@ -0,0 +1,13 @@
+//functions DistanceFormula 100pts Nico Liao
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+int main(){
+    for(int i=0; i<6; i++) {
+        for(int j=0; j<6; j++) {
+            cout << "(" << i << "," << j << ")";
+        }
+        cout << endl;
+    }
+}
