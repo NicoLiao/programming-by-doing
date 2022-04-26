86ee7ed9fc59478b173ce229fb89cd7487f0c5c4
NicoLiao <89239543+NicoLiao@users.noreply.github.com>
2022-04-26 14:00:22 +0800

    add file

--- C++/CppTest/Sorting2DArray.cpp
+++ C++/CppTest/Sorting2DArray.cpp
@@ -56,4 +56,41 @@ for(int m = 0; m < numRows*numCols; m++) {
         }
         cout << endl;
     }
+for(int m = 0; m < numRows*numCols; m++) {
+        for(int k = 0; k < numRows; k++) {
+            for(int l = 0; l < numCols-1; l++) {
+                
+                if(AR[k][l] < AR[k][l+1]) {
+                    temp = AR[k][l];
+                    AR[k][l] = AR[k][l+1];
+                    AR[k][l+1] = temp;
+                }
+            }
+        }
+            
+    for(int o = 0; o < numCols; o++) {
+        for(int p = 0; p < numRows-1; p++) {
+            if(o%2 == 0) {
+                if(AR[p][o] < AR[p+1][o]) { 
+                    temp = AR[p][o];
+                    AR[p][o] = AR[p+1][o];
+                    AR[p+1][o] = temp;
+                }
+            }else {
+                if(AR[p][o] > AR[p+1][o]) {
+                    temp = AR[p][o];
+                    AR[p][o] = AR[p+1][o]; 
+                    AR[p+1][o] = temp;
+                }
+            }
+        }
+    }
+}
+    cout << "Array after sorting" << endl;
+        for(int i = 0; i < numRows; i++) {
+            for(int j = 0; j < numCols; j++) {
+                cout << char(AR[i][j]) << " ";
+            }
+            cout << endl;
+        }
 }
